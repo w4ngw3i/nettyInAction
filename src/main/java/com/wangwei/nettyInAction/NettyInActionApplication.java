fd56c15c30f481b74a5a7bc25f8838cdb5e13fd3
wangweiobs <wangweiobs@foxmail.com>
2019-01-20 15:05:21 +0800

    add new file

---
+++ src/main/java/com/wangwei/nettyInAction/NettyInActionApplication.java
@@ -0,0 +1,14 @@
+package com.wangwei.nettyInAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyInActionApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/wangwei/nettyInAction/NettyInActionApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wangwei.nettyInAction;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NettyInActionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
